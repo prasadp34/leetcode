c8487e1811dfd61efededb30c16bc0bec5327a9d
Prasad Patil <99496789+prasadp34@users.noreply.github.com>
2024-04-11 00:14:35 +0530

    Time: 3 ms (67.33%), Space: 10.9 MB (34.66%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
-        sort(deck.begin(), deck.end()); // Sort the deck in increasing order
-        
-        int n = deck.size();
-        vector<int> result(n);
-        deque<int> indices;
-        
-        for (int i = 0; i < n; i++) {
-            indices.push_back(i); // Initialize deque with indices 0, 1, 2, ..., n-1
+        int n=deck.size();
+        sort(deck.begin(),deck.end());
+        vector<int>ans(n);
+        deque<int>dq;
+        for(int i=0;i<n;i++){
+            dq.push_back(i);
         }
-        
-        for (int card : deck) {
-            int idx = indices.front(); // Get the next available index
-            indices.pop_front(); // Remove the index from the front
-            result[idx] = card; // Place the card in the result array
-            if (!indices.empty()) {
-                indices.push_back(indices.front()); // Move the used index to the end of deque
-                indices.pop_front(); // Remove the index from the front
+        int chance=0;
+        int flip=0;
+        while(dq.size()>0){
+            if(flip==0){
+                ans[dq.front()]=deck[chance++];
+                dq.pop_front();
+            }
+            else{
+               int last=dq.front();
+                dq.pop_front();
+                dq.push_back(last);
+            }
+            if(flip==0){
+                flip=1;
+            }else{
+                flip=0;
             }
         }
+        return ans;
         
-        return result;
     }
 };
